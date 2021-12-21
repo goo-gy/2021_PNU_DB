308d0a3f707d00ea53bd5881d382475f2237db6f
goo-gy <lkukh17@naver.com>
2021-12-22 04:39:11 +0900

    complete functions

--- DB-Assignment2/covid/myApp/views.py
+++ DB-Assignment2/covid/myApp/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, resolve_url
 from django.db import connection
 
 # sqlQuery = "SELECT studentID, name, score, county FROM Students;"
+
+dict_query = {
+    "query1":"SELECT county, AVG(score) FROM students GROUP BY county ORDER BY county;",
+    "query2":"SELECT city, AVG(score) FROM (SELECT studentID, score, city FROM students JOIN counties ON students.county = counties.countyName ) AS studentCounty GROUP BY city ORDER BY city;",
+    "query3":"""SELECT topP.name, topS.name FROM 
+( SELECT name, county FROM professors p1 WHERE NOT EXISTS (SELECT * FROM professors WHERE county=p1.county AND age > p1.age AND facultyID<>p1.facultyID) ) AS topP
+JOIN
+( SELECT name, county FROM students s1 WHERE NOT EXISTS (SELECT * FROM students WHERE county=s1.county AND score > s1.score AND studentID<>s1.studentID) ) AS topS
+ON topS.county=topP.county ORDER BY topS.county;""",
+    "query4": """SELECT topP.name, topS.name FROM
+( SELECT name, city FROM students s1, counties c1 WHERE s1.county=c1.countyName AND NOT EXISTS (SELECT * FROM students, counties WHERE county=countyName AND city=c1.city AND studentID<>s1.studentID AND score > s1.score) ) AS topS,
+( SELECT name, city FROM professors s1, counties c1 WHERE s1.county=c1.countyName AND NOT EXISTS (SELECT * FROM professors, counties WHERE county=countyName AND city=c1.city AND facultyID<>s1.facultyID AND age > s1.age) ) AS topP
+WHERE topS.city=topP.city ORDER BY topS.city;""",
+    "query5":"""SELECT name, city FROM students JOIN
+(SELECT countyName, Counties.city FROM Counties JOIN
+(SELECT popCOVID.city, popCOVID.num/popCity.num as RATIO FROM 
+(SELECT city, SUM(population) as num FROM Counties group by city) AS popCity,
+(SELECT city, COUNT(*) as num FROM COVID GROUP BY city) AS popCOVID
+WHERE popCity.city=popCOVID.city order by RATIO DESC LIMIT 3) AS topCity
+ON Counties.city=topCity.city) as topCounty
+ON county=countyName;
+""",
+}
 
 def saveWithCSV(table, file):
     content = file.read().decode('utf8')
@@ -17,9 +40,22 @@ def saveWithCSV(table, file):
         insertSQL += ",".join(li_strTuple) + ';'
         cursor.execute(insertSQL)
 
+def getData(queryNum):
+    with connection.cursor() as cursor:
+        cursor.execute(dict_query[queryNum])
+        result = cursor.fetchall()
+        print(result)
+    return result
+
 def home(request):
     if(request.method == 'GET'):
-        return render(request, 'myApp/home.html')
+        try:
+            queryNum = request.GET['queryNum']
+            if(queryNum in dict_query.keys()):
+                result = getData(queryNum)
+                return render(request, 'myApp/home.html', {"data": result})
+        except:
+            return render(request, 'myApp/home.html')
     elif(request.method == 'POST'):
         if(len(request.FILES)):
             li_name = ['Students', 'Professors', 'Counties', 'COVID']
@@ -31,7 +67,7 @@ def home(request):
                     continue
                 try:
                     saveWithCSV(name, file)
-                    return render(request, 'myApp/home.html', {"msg": "%s가 등록되었습니다." %name})
+                    return render(request, 'myApp/home.html', {"msg" : "%s가 등록되었습니다." %name})
                 except:
-                    return render(request, 'myApp/home.html', {"error": {"msg":"등록에 실패하였습니다.(중복/형식)"}})
-        return render(request, 'myApp/home.html', {"error": {"msg":"파일을 업로드하여 주십시오."}})
+                    return render(request, 'myApp/home.html', {"error": {"msg" : "등록에 실패하였습니다.(중복/형식)"}})
+        return render(request, 'myApp/home.html', {"error" : {"msg":"파일을 업로드하여 주십시오."}})
